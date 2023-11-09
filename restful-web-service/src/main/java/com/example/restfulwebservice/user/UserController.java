91217523be41e63eea5c41fd1e7d035c076c9c1a
kunwoo park <scv74502@gmail.com>
2023-11-10 01:29:12 +0900

    http status code control, servleturicomponentsbuilder method

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,7 +27,15 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = service.save(user);
+
+        // 사용자의 주소값 반환하기
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
