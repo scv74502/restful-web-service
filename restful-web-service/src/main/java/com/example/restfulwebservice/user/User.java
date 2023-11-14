d6271679d23d6b4dc23125d93eaefaef563eeaf1
kunwoo park <scv74502@gmail.com>
2023-11-15 01:31:57 +0900

    Validation api, review CustomizedResponseEntityExceptionHandler, ExceptionResponse

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,12 +3,19 @@ package com.example.restfulwebservice.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 public class User {
     private Integer id;
+
+    @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
+
+    // 현재 시간보다 과거 시간만 사용할 수 있도록 제한함
+    @Past
     private Date joinDate;
 }
--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -34,7 +35,7 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
         User savedUser = service.save(user);
 
         // 사용자의 주소값 반환하기
