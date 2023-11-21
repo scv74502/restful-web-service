1417cf3dc077b714fc017e5cf6b63240d5a6da3f
kunwoo park <scv74502@gmail.com>
2023-11-21 17:24:02 +0900

    Enable hateoas

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice.user;
 
-import com.sun.istack.ByteArrayDataSource;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -24,14 +28,20 @@ public class UserController {
 
     // GET /users/{Integer id}
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
         if (user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
 
+        // HATEOAS
+        // "all-users", SERVER_PATH +"/users"
+        // retrieveAllUsers
+        EntityModel<User> model = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());    // static method call
+        model.add(linkTo.withRel("all-users"));
 
-        return user;
+        return model;
     }
 
     @PostMapping("/users")
