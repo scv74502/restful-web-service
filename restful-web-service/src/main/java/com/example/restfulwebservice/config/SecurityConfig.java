9819994eef0f155c28d2ec4d1a097513e157e72d
kunwoo park <scv74502@gmail.com>
2023-12-19 01:56:01 +0900

    data.sql, security ignore. 4/10

--- restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -1,31 +1,41 @@
-//package com.example.restfulwebservice.config;
-//
-//import com.example.restfulwebservice.dao.UserDaoService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    UserDetailsService userDetailsService(){
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//
-//        UserDetails newUser = User.withUsername("user")
-//                .password(passwordEncoder().encode("password"))
-//                .authorities("read")
-//                .build();
-//
-//        userDetailsManager.createUser(newUser);
-//        return userDetailsManager;
-//    }
-//
-//    @Bean
-//    BCryptPasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.example.restfulwebservice.config;
+
+import com.example.restfulwebservice.dao.UserDaoService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails newUser = User.withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .authorities("read")
+                .build();
+
+        userDetailsManager.createUser(newUser);
+        return userDetailsManager;
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    // 사용하고자 하는 url에서 무인증으로 사용 가능한 부분
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        // 인증 무시하는 로직, webflux나 reactive 부분에서 사용되는 체이닝 코드
+        return (web) -> web.ignoring().
+                requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+}
---
+++ restful-web-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values(90001, now(), 'User1', 'test111', '701010-1111111');
+insert into users(id, join_date, name, password, ssn) values(90002, now(), 'User2', 'test112', '701010-1111112');
+insert into users(id, join_date, name, password, ssn) values(90003, now(), 'User3', 'test113', '701010-1111113');
